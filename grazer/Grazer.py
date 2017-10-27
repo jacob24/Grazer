ef8d14598720e01e278b23a9220e7b535e8aa628
jacob24 <abello.jacob@gmail.com>
2017-10-27 11:36:43 +0800

    main data holder for grazer

--- grazer/Grazer.py
+++
@@ -1,2 +0,0 @@
-class Grazer(object):
-    pass
---
+++ grazer/data.py
@@ -0,0 +1,19 @@
+class GrazerData(object):
+
+    def __init__(self, key, **kwargs):
+        self.key = key
+        self.meta_data = {}
+
+    def add_metadata(self, key, value):
+        if key not in self.meta_data.keys():
+            _values = [value]
+            self.meta_data.update({key:_values})
+        else:
+            for _val in self.meta_data[key]:
+                if _val == value:
+                    return
+
+            self.meta_data[key].append(value)
+
+    def get_metadata(self):
+        return self.meta_data
