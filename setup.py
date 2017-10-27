4cc14b7ad3f4127470c27f2a33e5f93987c829f1
jacob24 <abello.jacob@gmail.com>
2017-10-27 11:38:12 +0800

    updated

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 DESCRIPTION = 'Grazer'
-LONG_DESCRIPTION = 'Feed me data so I can produce good result'
+LONG_DESCRIPTION = 'Feed me data and I will produce good result'
 
 setup(
     name='grazer',
     version='0.1',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author='abello.jacob@gmail.com'
+    author='Jacob Abello'
 )
